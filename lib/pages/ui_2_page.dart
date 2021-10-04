9e957c3744a13b361e6f8ef2d9bf53f38c459cf6
Gian Carlos <gpaucarcortez@gmail.com>
2021-10-04 12:32:40 -0500

    Table

--- lib/pages/ui_2_page.dart
+++ lib/pages/ui_2_page.dart
@@ -10,7 +10,41 @@ class UI2Page extends StatelessWidget {
       body: Stack(
         children: [
           buildBackground(),
-          buildTitle(),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                buildTitle(),
+                Table(
+                  children: [
+                    TableRow(
+                      children: [
+                        buildContainerItem(),
+                        buildContainerItem(),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        buildContainerItem(),
+                        buildContainerItem(),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        buildContainerItem(),
+                        buildContainerItem(),
+                      ],
+                    ),
+                    TableRow(
+                      children: [
+                        buildContainerItem(),
+                        buildContainerItem(),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -28,33 +62,63 @@ class UI2Page extends StatelessWidget {
     );
   }
 
+  Container buildContainerItem() {
+    return Container(
+      height: 180,
+      margin: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          color: Color(0xff515593).withOpacity(0.75),
+          borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 36.0,
+            child: Icon(Icons.people, size: 34.0, color: Colors.white),
+            backgroundColor: Color(0xffFF4081),
+          ),
+          SizedBox(
+            height: 6.0,
+          ),
+          Text(
+            "Contact",
+            style: TextStyle(
+              color: Color(0xffFF4081),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   Widget buildTitle() {
     return SafeArea(
-
       child: Padding(
         padding: EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Classify transaction",
-                  style: TextStyle(
-                    fontSize: 28,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Text(
-                  "Classify this transaction into a particular category",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ],
+          children: [
+            Text(
+              "Classify transaction",
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
             ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Classify this transaction into a particular category",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
